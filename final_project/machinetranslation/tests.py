8fa9dcee93100072cd0c89c72ad4d483bf8c7913
kingfroglao <xzlao@ucdavis.edu>
2023-07-12 16:06:37 -0700

    Task 9

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -8,6 +8,11 @@ class TestE2F(unittest.TestCase):
     
     def test1(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertEqual(english_to_french('Bye'), 'Au revoir')
+        
+    
+    def test2(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertEqual(french_to_english('Au revoir'), 'Bye')
 
 unittest.main()
